6a39f90417acb7f065fdab8c69485a598364c866
tanianvzz <noviantitania585@gmail.com>
2023-11-13 14:37:02 -0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/git/belajar1/';
+$config['base_url'] = 'http://localhost/GIT/belajar1/contact';
 
 /*
 |--------------------------------------------------------------------------
